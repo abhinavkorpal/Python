98885862e572367c0e4ce6dcae7ed08636832d0f
Abhinav Korpal <abhinavkorpaljv@gmail.com>
2018-12-22 23:51:11 +0530

    AWS Python

--- AWS_Chalice/helloworld/app.py
+++ AWS_Chalice/helloworld/app.py
@@ -1,12 +1,102 @@
-from chalice import Chalice
+import sys
+from chalice import Chalice, Response
+# if sys.version_info[0] == 3:
+#     from urllib.parse import urlparse, parse_qs
+# else:
+#     from urlparse import urlparse, parse_qs
+from chalice import BadRequestError
+from chalice import UnauthorizedError
+from chalice import NotFoundError
+from chalice import CORSConfig
+
+cors_config = CORSConfig(
+    allow_origin='https://foo.example.com',
+    allow_headers=['X-Special-Header'],
+    max_age=600,
+    expose_headers=['X-Special-Header'],
+    allow_credentials=True
+)
+
+_ALLOWED_ORIGINS = set([
+    'http://allowed1.example.com',
+    'http://allowed2.example.com',
+])
 
 app = Chalice(app_name='helloworld')
 
+CITIES_TO_STATE = {
+    'seattle': 'WA',
+    'portland': 'OR',
+}
+
+OBJECTS = {
+}
 
 @app.route('/')
 def index():
     return {'hello': 'world'}
+    # return Responsebody('hello world!', status_code=200, headers={'Content-Type': 'text/plain'})
 
+@app.route('/cities/{city}')
+def state_of_city(city):
+    try:
+        return {'state': CITIES_TO_STATE[city]}
+    except KeyError:
+        raise BadRequestError("Unknown city '%s', valid choices are: %s" % (
+            city, ', '.join(CITIES_TO_STATE.keys())))
+
+@app.route('/resource/{value}', methods=['PUT'])
+def put_test(value):
+    return {"value": value}
+
+@app.route('/myview', methods=['POST', 'PUT'])
+def myview():
+    pass
+
+@app.route('/objects/{key}', methods=['GET', 'PUT'])
+def myobject(key):
+    request = app.current_request
+    if request.method == 'PUT':
+        OBJECTS[key] = request.json_body
+    elif request.method == 'GET':
+        try:
+            return {key: OBJECTS[key]}
+        except KeyError:
+            raise NotFoundError(key)
+
+@app.route('/introspect')
+def introspect():
+    return app.current_request.to_dict()
+
+# @app.route('/', methods=['POST'], content_types=['application/x-www-form-urlencoded'])
+# def index():
+#     parsed = parse_qs(app.current_request.raw_body.decode())
+#     return {
+#         'states': parsed.get('states', [])
+#     }
+
+@app.route('/custom_cors', methods=['GET'], cors=cors_config)
+def supports_custom_cors():
+    return {'cors': True}
+
+@app.route('/cors_multiple_origins', methods=['GET', 'OPTIONS'])
+def supports_cors_multiple_origins():
+    method = app.current_request.method
+    if method == 'OPTIONS':
+        headers = {
+            'Access-Control-Allow-Method': 'GET,OPTIONS',
+            'Access-Control-Allow-Origin': ','.join(_ALLOWED_ORIGINS),
+            'Access-Control-Allow-Headers': 'X-Some-Header',
+        }
+        origin = app.current_request.headers.get('origin', '')
+        if origin in _ALLOWED_ORIGINS:
+            headers.update({'Access-Control-Allow-Origin': origin})
+        return Response(
+            body=None,
+            headers=headers,
+        )
+    elif method == 'GET':
+        return 'Foo'
 
 # The view function above will return {"hello": "world"}
 # whenever you make an HTTP GET request to '/'.
