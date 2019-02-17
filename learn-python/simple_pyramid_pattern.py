3c69710f233fb17a45109ad6923304be3bfe1eef
Korpal, Abhinav <abhinav.korpal@accenture.com>
2019-02-17 18:34:53 +0530

    Initial commit

--- learn-python/simple_pyramid_pattern.py
+++ learn-python/simple_pyramid_pattern.py
@@ -1,5 +1,8 @@
+# Patterns can be printed in python using simple for loops. 
+# First outer loop is used to handle number of rows and Inner nested loop is used to handle the number of columns. 
+# Manipulating the print statements, different number patterns, alphabet patterns or star patterns can be printed.
+
 # Python 3.x code to demonstrate star pattern 
-  
 # Function to demonstrate printing pattern 
 def pypart(n): 
       
@@ -20,3 +23,17 @@ def pypart(n):
 # Driver Code 
 n = 5
 pypart(n)
+
+# Another Approach:
+# Using List in Python 3, this could be done in a simpler way
+
+def pypart1(m):
+    myList = []
+    for i in range(1,m+1):
+        myList.append("* "*i)
+    print("\n".join(myList))
+
+m = 5
+pypart1(m)
+
+
